434f8f6931c3b8607431c4269ab31a26ae91317b
Sid-Agnihotri <56989925+Sid-Agnihotri@users.noreply.github.com>
2021-02-07 19:51:04 -0800

    Final push
    App complete.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
 import Job from "./components/Job";
 import JobPagination from "./components/JobPagination";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   const [params, setparams] = useState({});
   const [page, setpage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  const handelParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setpage(1);
+    setparams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs </h1>
+      <SearchForm
+        params={params}
+        onParamChange={handelParamChange}
+      ></SearchForm>
       <JobPagination
         page={page}
         setPage={setpage}
